e58a4fb980dfc2b1db813e3f7fd3fb6432046217
Méloé <meloscavezzoni@gmail.com>
2019-11-18 10:46:24 +0100

    Triangle + color

--- Exo-3/script.js
+++ Exo-3/script.js
@@ -9,19 +9,47 @@ const border = () => {
   context.strokeRect(0, 0, 500, 500); // Rectangle vide
 };
 
-const fullTriangle = () => {
-  context.stokeStyle = "red";
-  context.beginPath();
+const emptyTriangle = () => {
+  context.lineWidth = "3"; // épaisseur ligne
+  context.strokeStyle = "red"; // couleur ligne
+  context.beginPath(); // début dessin
   context.moveTo(10, 10);
   context.lineTo(10, 100);
   context.lineTo(100, 10);
+  context.closePath(); // fin dessin
+  context.stroke(); // On dessine
+};
+
+const fullTriangle = () => {
+  let lineargradient = context.createLinearGradient(100, 200, 150, 100);
+  lineargradient.addColorStop(0, "blue");
+  lineargradient.addColorStop(1, "purple");
+
+  context.fillStyle = lineargradient;
+  context.beginPath();
+  context.moveTo(100, 200);
+  context.lineTo(200, 200);
+  context.lineTo(150, 80);
+  context.fill();
+};
+
+const emptyTriangle2 = () => {
+  context.lineWidth = "5";
+  context.strokeStyle = "green";
+  context.lineJoin = "round";
+  context.beginPath();
+  context.moveTo(200, 300);
+  context.lineTo(400, 300);
+  context.lineTo(350, 200);
   context.closePath();
   context.stroke();
 };
 
 const draw = () => {
   border();
+  emptyTriangle();
   fullTriangle();
+  emptyTriangle2();
 };
 
 draw();
