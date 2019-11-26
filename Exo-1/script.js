e7d8d3bdf633bd2139609fcc7549b3191bc1625f
Méloé <meloscavezzoni@gmail.com>
2019-11-26 14:07:59 +0100

    ex1

--- Exo-1/script.js
+++ Exo-1/script.js
@@ -1,54 +1,80 @@
-const canvas = document.getElementById("canvas"); // On récupère le canvas
-const context = canvas.getContext("2d"); // On récupère le contexte
+const canvas = document.getElementById("canvas"); // We recover the canvas
+const context = canvas.getContext("2d"); // We recover the context
+const width = canvas.getAttribute("width"); // Width canvas
+const height = canvas.getAttribute("height"); // Height canvas
 
-const drawImg = () => {
-  const width = canvas.getAttribute("width"); // largueur canvas
-  const height = canvas.getAttribute("height"); // hauteur canvas
-  const img = new Image(); // création image
-  img.src = "chaton.jpg"; // chemin image
+// Draw the image
+const drawImg = (
+  pathSrc,
+  cutX,
+  cutY,
+  cutWidth,
+  cutHeight,
+  imgWidth,
+  imgHeight
+) => {
+  const img = new Image(); // Creating the image
+  img.src = pathSrc; // Path of the image
+
   img.onload = () => {
-    // chargement
+    // Loading
     context.drawImage(
-      // on dessine
-      img, // image
-      200, // découpage image
-      10,
-      400, //largueur image
-      400, //hauteur image
-      width / 2 - 100, // pour mettre au centre : (taille canvas /2) - (taille image /2)
-      height / 2 - 100,
-      200, // taille image
-      200
+      // We draw
+      img, // Image
+      cutX, // Cutting the image
+      cutY,
+      cutWidth, // Width image
+      cutHeight, // Height image
+      width / 2 - imgWidth / 2, // To put in the center: (size canvas /2) - (size image /2)
+      height / 2 - imgHeight / 2,
+      imgWidth, // size image
+      imgHeight
     );
   };
 };
 
+// Draw the border
 const drawBorder = () => {
-  context.stokeStyle = "green";
-  context.strokeRect(0, 0, 500, 500); // Rectangle vide
+  context.lineWidth = "5";
+  context.strokeStyle = "green";
+  context.strokeRect(0, 0, width, height); // Empty rectangle
 };
 
-const drawRect = () => {
+// Draw the title
+const drawTitle = (width, y) => {
+  let text = "Exercice 1";
+  let mesure = context.measureText(text).width;
   context.font = "20px serif";
-  context.fillText("Exercice 1", 10, 20); // texte plein
-
-  context.fillStyle = "red";
-  context.fillRect(5, 70, 50, 50); // Rectangle plein
-
-  context.fillStyle = "blue";
-  context.fillRect(80, 70, 50, 50);
-
-  context.fillStyle = "green";
-  context.fillRect(150, 70, 50, 50);
-
-  context.fillStyle = "yellow";
-  context.fillRect(220, 70, 50, 50);
+  context.fillText(text, width / 2 - mesure, y);
 };
 
+// Draw the filled rectangle
+const drawRectFill = (color, x, y, width, height) => {
+  context.fillStyle = color;
+  context.fillRect(x, y, width, height); // Rectangle plein
+};
+
+// Draw the empty rectangle
+const drawRectStroke = (color, x, y, width, height) => {
+  context.lineWidth = "3";
+  context.strokeStyle = color;
+  context.strokeRect(x, y, width, height);
+};
+
+// Draw all
 const draw = () => {
-  drawRect();
-  drawImg();
+  // Draw border
   drawBorder();
+  // Title
+  drawTitle(width, 30);
+  // The rectangles
+  drawRectFill("red", 20, 70, 50, 50);
+  drawRectStroke("blue", 95, 70, 50, 50);
+  drawRectFill("green", 165, 70, 50, 50);
+  drawRectStroke("yellow", 235, 70, 50, 50);
+  // Draw image in center
+  // Src - cutX - cutY - cutWidth - cutHeight - ImgWidth - ImgHeight
+  drawImg("chaton.jpg", 200, 10, 400, 400, 200, 200);
 };
 
 draw();
